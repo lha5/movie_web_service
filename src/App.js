b4f29ff66146a4480c65a7cd4c52aa4476a8b6b1
lha5 <hyeahnlee5@gmail.com>
2020-09-29 17:27:37 +0900

    Grommet UI 추가 및 적용 중

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './general.css'
+import {Box, Grid} from "grommet";
 
 class App extends React.Component {
     state = {
         isLoading: true,
         movieList: [],
     };
-
-    // yyyymmdd 포맷
-    getFormatDate = (date) => {
-        let year = date.getFullYear();
-        let month = (1 + date.getMonth());
-        month = month >= 10 ? month : '0' + month;
-        let day = date.getDate();
-        day = day >= 10 ? day : '0' + day;
-        return year + month + day;
-    }
 
     getMovies = async () => {
         const key = '0f964f1c444c706954dd15c2853e5d97';
@@ -27,7 +19,6 @@ class App extends React.Component {
         } = await axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key='+key+'&language=ko&page=1&region=KR');
         console.log(movieList);
         this.setState({movieList, isLoading: false});
-        // console.log(await axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key='+key+'&language=ko&page=1&region=KR'));
     }
 
     componentDidMount() {
@@ -36,7 +27,7 @@ class App extends React.Component {
 
     render() {
         const {isLoading, movieList} = this.state;
-        return <div>{isLoading ? "Loading" : movieList.map((movie, index) => {
+        const movie_data = movieList.map((movie, index) => {
             return (
                 <Movie
                     key={index}
@@ -45,8 +36,34 @@ class App extends React.Component {
                     overview={movie.overview}
                     poster_path={movie.poster_path}
                     release_date={movie.release_date}
-                />);
-        })}</div>
+                />
+            )
+        });
+
+        return <Box>
+            {
+                isLoading ?
+                <div class="loader">
+                    <span class="loader_text">loading...</span>
+                </div>
+                : (
+                    <Grid
+                        rows={['xsmall', 'auto']}
+                        columns={['small', 'auto']}
+                        areas={[
+                            {name: 'header', start: [0, 0], end: [1, 0]},
+                            {name: 'nav', start: [0, 1], end: [0, 1]},
+                            {name: 'main', start: [1, 1], end: [1, 1]},
+                        ]}
+                    >
+                        <Box gridArea="header" background="brand"></Box>
+                        <Box gridArea="nav" background="light-2"></Box>
+                        <Box gridArea="main" background="dark-3" pad="medium">
+                            {movie_data}
+                        </Box>
+                    </Grid>
+                )
+            }</Box>
     }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Box, Grid} from "grommet";
 
-function Movie({id, title, overview, poster_path}) {
-    return <h1>{title}</h1>
+function Movie({id, title, overview, poster_path, release_date}) {
+    return <Grid columns={{count: "fill"}}>
+        <Box
+            direction="column"
+            border={{color: 'pink', size: 'xsmall'}}
+            align="center"
+            width="250px"
+            margin="none"
+        >
+            <Box pad="none"><img src={"https://image.tmdb.org/t/p/w200/" + poster_path} alt={title}/></Box>
+            <Box pad="none" background={"light-3"} fill="horizontal">{title}</Box>
+            <Box pad="none" background={"light-3"} fill="horizontal">{release_date}</Box>
+        </Box>
+    </Grid>
 }
 
 Movie.propTypes = {
