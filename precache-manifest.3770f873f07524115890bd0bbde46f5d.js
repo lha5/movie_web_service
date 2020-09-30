84bb45d94e56f0d3614942ebf06a4478a5ad456d
lha5 <hyeahnlee5@gmail.com>
2020-09-30 14:41:19 +0900

    Updates

---
+++ precache-manifest.3770f873f07524115890bd0bbde46f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3cb38fc7417b0526e250535cdd5a3cd",
+    "url": "/movie_web_service/index.html"
+  },
+  {
+    "revision": "4be1e03a7513344105ad",
+    "url": "/movie_web_service/static/css/main.155e49e2.chunk.css"
+  },
+  {
+    "revision": "2060e2466cf33ff08cf5",
+    "url": "/movie_web_service/static/js/2.78d10c64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_web_service/static/js/2.78d10c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be1e03a7513344105ad",
+    "url": "/movie_web_service/static/js/main.f840295c.chunk.js"
+  },
+  {
+    "revision": "5e42be708f91f0b26f15",
+    "url": "/movie_web_service/static/js/runtime-main.ad250604.js"
+  }
+]);
